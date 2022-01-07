16465f94d97e2b165edb621ebbbefdaa9c96d538
Murat Bilgili <muratbilgili34@gmail.com>
2022-01-07 16:31:24 +0000

    ruby development
    new features added

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float speedEnemy = 0.94f;
+    [SerializeField] private bool vertical = true;
+    [SerializeField] private float changeTime = 1.26f;
+    Rigidbody2D rigidbody2D;
+    Animator animator;
+    float timer;
+    int direction = 1;
+    bool isBroken = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!isBroken)
+        {
+            return;
+        }
+        Vector2 position = rigidbody2D.position;
+
+        if (vertical)
+        {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+            position.y = position.y + Time.deltaTime * speedEnemy * direction; ;
+        }
+        else
+        {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+            position.x = position.x + Time.deltaTime * speedEnemy * direction; ;
+        }
+
+        rigidbody2D.MovePosition(position);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+    public void Fix() 
+    {
+        isBroken = false;
+        rigidbody2D.simulated = false; //This removes the Rigidbody from the Physics System simulation, so it won’t be taken into account by the system for collision, and the fixed robot won’t stop the Projectile anymore or be able to hurt the main character.
+        animator.SetBool("Fixed", true);
+    }
+}
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,25 +5,98 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     [SerializeField] private float speed = 5 ;
+    Rigidbody2D rigidbody2d;
+    Animator rubyAnimator;
+    public GameObject projectilePrefab;
     private float horizontalInput;
     private float verticalInput;
+
+    public int maxHealth = 5;
+    public int currentHealth;
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    public int health 
+    { 
+        get 
+        { 
+            return currentHealth; 
+        } 
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        rubyAnimator = GetComponent<Animator>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-         horizontalInput = Input.GetAxis("Horizontal");
-         verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontalInput, verticalInput);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        rubyAnimator.SetFloat("Look X", lookDirection.x);
+        rubyAnimator.SetFloat("Look Y", lookDirection.y);
+        rubyAnimator.SetFloat("Speed", move.magnitude);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Launch();
+        } 
+        
     }
-    // Update is called once per frame
+
     void FixedUpdate()
     {
-       
-        gameObject.transform.Translate(Vector2.right * horizontalInput * speed * Time.deltaTime);
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 3.0f * horizontalInput * Time.deltaTime;
+        position.y = position.y + 3.0f * verticalInput * Time.deltaTime;
 
-        
-        gameObject.transform.Translate(Vector2.up * verticalInput * speed * Time.deltaTime);
+        rigidbody2d.MovePosition(position);
     }
+
+    public void ChangeHealth(int amount)
+    {
+
+        if (amount < 0)
+        {
+            rubyAnimator.SetTrigger("Hit");
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        rubyAnimator.SetTrigger("Launch");
+    }
+
 }
